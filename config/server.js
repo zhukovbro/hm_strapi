29343a90cbc7c9a6c566e5d91d96919e7ad0f1d6
rey317 <azhykov@gmail.com>
2024-06-23 18:18:12 +0200

    deploy.yml changed test strapi 17

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('URL', 'http://94.198.216.72/strapi'),
   app: {
     keys: env.array('APP_KEYS'),
   },
