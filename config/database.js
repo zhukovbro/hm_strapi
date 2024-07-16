78eac1a38083bafb8c997fc24c1e6de5fc9a9272
rey317 <azhykov@gmail.com>
2024-07-16 20:14:09 +0200

    deploy.yml changed to PostgreSQL27

--- config/database.js
+++ config/database.js
@@ -28,7 +28,7 @@ module.exports = ({ env }) => {
     },
     mysql2: {
       connection: {
-        host: env('DATABASE_HOST', 'localhost'),
+        host: env('DATABASE_HOST', '94.198.216.72'),
         port: env.int('DATABASE_PORT', 3306),
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
