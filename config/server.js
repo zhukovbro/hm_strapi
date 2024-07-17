26927cedf3cae0cfb3ca1486f07b0e31b88aae5a
rey317 <azhykov@gmail.com>
2024-07-17 12:10:33 +0200

    deploy.yml changed to PostgreSQL32

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('URL', 'http://94.198.216.72/strapi'),
+  url: env('URL', 'strapi'),
   app: {
     keys: env.array('APP_KEYS'),
   },
